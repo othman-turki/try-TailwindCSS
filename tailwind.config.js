9f0815384f85eb70119b1bba1e160e7e9960d5a8
Othman Turki <othmanturki.dev@gmail.com>
2022-01-13 14:05:09 +0100

    Push try TailwindCSS project to github

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
